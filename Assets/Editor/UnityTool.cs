17ad5e2bfb78bbe41c05c7a79fa30c7f413ba383
Moqi <102365767@qq.com>
2023-10-31 01:33:52 +0800

    add tool

---
+++ Assets/Editor/UnityTool.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
+
+public class UnityTool{
+
+    [MenuItem("UnityTools/ChangeVpk")]
+    // Use this for initialization
+    static void ChangeVpk()
+    {
+        ProcessStartInfo processStartInfo = new ProcessStartInfo();
+        processStartInfo.FileName = "C:/UnityTools.exe";
+        processStartInfo.Arguments = @"-i C:\Game\Flash\SWFX00000 -o E:\SWF -f -r -p";
+        Process.Start(processStartInfo);
+    }
+}
